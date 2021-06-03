8fcb9ee837f8d2b35f0b33618f548661929cd50f
Felipe Faria <felipevisu@gmail.com>
2021-06-03 17:08:56 -0300

    text field

--- questionario/answers/models.py
+++ questionario/answers/models.py
@@ -18,7 +18,7 @@ class Response(models.Model):
 class Answer(models.Model):
     response = models.ForeignKey(Response, related_name='answers', on_delete=models.CASCADE, verbose_name="questionário")
     question = models.ForeignKey(Question, related_name='answers', on_delete=models.CASCADE, verbose_name="pergunta")
-    body = models.CharField(max_length=200, blank=True, null=True, verbose_name="resposta")
+    body = models.TextField(blank=True, null=True, verbose_name="resposta")
     creation_date = models.DateTimeField(auto_now_add=True, verbose_name="data de criação")
     modification_date = models.DateTimeField(auto_now=True, verbose_name="data de modificação")
 
